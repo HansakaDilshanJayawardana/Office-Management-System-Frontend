91893cffff0125941f53965bcf73792bbf2d0639
Modeesha-Kalani <modeeshakalani97@gmail.com>
2023-03-21 23:57:06 +0530

    add employee page completed

--- screens/AddEmployee.js
+++ screens/AddEmployee.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
-  View,
+  View
 } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import Spacing from "../constants/Spacing";
@@ -13,67 +13,95 @@ import FontSize from "../constants/FontSize";
 import Colors from "../constants/Colors";
 import Font from "../constants/Font";
 import AppTextInput from "../components/AppTextInput";
+import axios from "axios";
+import { API_BASE_URL } from '../config';
+import { Picker } from '@react-native-picker/picker';
 
 const AddEmployee = ({ navigation: { navigate } }) => {
-  return (
-    <ScrollView>
-      <View
-        style={{
-          padding: Spacing * 2,
-        }}
-      >
-        <View
-            style={{
-                alignItems: "center",
-            }}
-        >
-            <Text
-    style={{
-      fontSize: FontSize.xLarge,
-      color: Colors.primary,
-      fontFamily: Font["poppins-bold"],
-      marginVertical: Spacing * 3,
-    }}
-  >
-    Add Employee
-            </Text>
-            <Text
-                style={{
-                fontFamily: Font["poppins-regular"],
-                fontSize: FontSize.small,
-                maxWidth: "80%",
-                textAlign: "center",
-                }}
-            >
-                Add New Employee 
-            </Text>
-        </View>
-        <View
-        style={{
-            marginVertical: Spacing * 3,
-        }}
-        >
-        <AppTextInput placeholder="Name" />
-        <AppTextInput placeholder="Gender" />
-        <AppTextInput placeholder="NIC Number" />
-        <AppTextInput placeholder="Address" />
-        <AppTextInput placeholder="Contact Number" />
-        </View>
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("");
+  const [nic, setNic] = useState("");
+  const [address, setAddress] = useState("");
+  const [contact, setContact] = useState("");
+  const [email, setEmail] = useState("");
 
-        <TouchableOpacity
-        style={{
+  const addEmployee = async () => {
+    const data = {
+      name: name,
+      gender: gender,
+      nic: nic,
+      address: address,
+      contact: contact,
+      email: email
+    };
+ 
+    await axios.post(API_BASE_URL+'/employee/add', data)
+    .then((res) => {
+      console.log(res.data);
+      if(res.data.success === true){
+        navigate("EmployeeHomeScreen")
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+return (
+    <ScrollView>
+      <View style={{ padding: Spacing * 2 }}>
+        <View style={{ alignItems: 'center' }}>
+          <Text style={{
+              fontSize: FontSize.xLarge,
+              color: Colors.primary,
+              fontFamily: Font["poppins-bold"],
+              marginVertical: Spacing * 3
+            }}>
+            Add Employee
+          </Text>
+          <Text style={{
+              fontFamily: Font["poppins-regular"],
+              fontSize: FontSize.small,
+              maxWidth: '80%',
+              textAlign: 'center'
+            }}>
+            Fill in the form with new Employee Details
+          </Text>
+        </View>
+        <View style={{ marginVertical: Spacing * 3 }}>
+          <AppTextInput placeholder="Name" onChangeText={(text) => setName(text)} />
+          <Picker
+            selectedValue={gender}
+            onValueChange={(value) => setGender(value)}
+            style={{ fontFamily: Font["poppins-regular"],
+              fontSize: FontSize.small, 
+              height: 65, padding: Spacing * 2, 
+              backgroundColor: Colors.lightPrimary, 
+              marginVertical: Spacing,}}
+          >
+            <Picker.Item label="Select Gender" value="" style={{color: Colors.darkText}} />
+            <Picker.Item label="Male" value="Male" />
+            <Picker.Item label="Female" value="Female" />
+          </Picker>
+          <AppTextInput placeholder="NIC" onChangeText={(text) => setNic(text)} />
+          <AppTextInput placeholder="Address" onChangeText={(text) => setAddress(text)} />
+          <AppTextInput placeholder="Contact Number" onChangeText={(text) => setContact(text)} />
+          <AppTextInput placeholder="Email" onChangeText={(text) => setEmail(text)} />
+        </View>
+        <TouchableOpacity style={{
             padding: Spacing * 2,
             backgroundColor: Colors.primary,
             marginVertical: Spacing * 3,
             borderRadius: Spacing,
             shadowColor: Colors.primary,
             shadowOffset: {
-            width: 0,
-            height: Spacing,
+              width: 0,
+              height: Spacing
             },
             shadowOpacity: 0.3,
-            shadowRadius: Spacing,
-        }}
+            shadowRadius: Spacing
+          }}
+          onPress={() => addEmployee()}
         >
         <Text
             style={{
@@ -86,26 +114,9 @@ const AddEmployee = ({ navigation: { navigate } }) => {
             Add Now
         </Text>
         </TouchableOpacity>
-        <TouchableOpacity
-        onPress={() => navigate("Login")}
-        style={{
-            padding: Spacing,
-        }}
-        >
-        <Text
-            style={{
-            fontFamily: Font["poppins-semiBold"],
-            color: Colors.text,
-            textAlign: "center",
-            fontSize: FontSize.small,
-            }}
-        >
-            View Employee
-        </Text>
-        </TouchableOpacity>
       </View>
     </ScrollView>
   );
 };
 
-export default AddEmployee
+export default AddEmployee;
